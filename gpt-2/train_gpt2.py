59234585cc6c7bed0724f46843434f56767862c4
Mihik197 <gea1500517@gmail.com>
2024-08-04 21:07:19 +0530

    modified DataLoaderLite for handling processed fineweb dataset

--- gpt-2/train_gpt2.py
+++ gpt-2/train_gpt2.py
@@ -235,24 +235,35 @@ class GPT(nn.Module):
 
 # DataLoader
 import tiktoken
+import numpy as np
+
+def load_tokens(filename):
+    npt = np.load(filename)
+    ptt = torch.tensor(npt, dtype=torch.long)
+    return ptt
 
 class DataLoaderLite:
-    def __init__(self, B, T, process_rank, num_processes):
+    def __init__(self, B, T, process_rank, num_processes, split):
         self.B = B
         self.T = T
         self.process_rank = process_rank 
         self.num_processes = num_processes
+        assert split in {'train', 'val'}
 
-        # at init load tokens from disk and store in memory
-        enc = tiktoken.get_encoding('gpt2')
-        with open('input.txt', 'r') as f:
-            text = f.read()
-        tokens = enc.encode(text)
-        self.tokens = torch.tensor(tokens)
-        print(f'loaded {len(self.tokens)} tokens')
-        print(f'1 epoch = {len(self.tokens) // (B * T)} batches')
+        # get the shard filenames
+        data_root = "edu_fineweb10B"
+        shards = os.listdir(data_root)
+        shards = [s for s in shards if split in s]
+        shards = sorted(shards)
+        shards = [os.path.join(data_root, s) for s in shards]
+        self.shards = shards
+        assert len(shards) > 0, f"no shards found for split {split}"
+        if master_process:
+            print(f"found {len(shards)} shards for split {split}")
 
-        # state
+        # state, init at shard zero
+        self.current_shard = 0
+        self.tokens = load_tokens(self.shards[self.current_shard])
         self.current_position = self.B * self.T * self.process_rank
 
     def next_batch(self):
@@ -262,8 +273,11 @@ class DataLoaderLite:
         y = buf[1:].view(B, T)  # targets
         # advance the position in the tensor
         self.current_position += B * T * self.num_processes  # the position will advance by the entire chunk (0 -> 8, 1 -> 9, etc)
+        # if loading the next batch would be out of bounds, advance to the next shard
         if self.current_position + (B * T * self.num_processes + 1) > len(self.tokens):
-            self.current_position = self.B * self.T * self.process_rank
+            self.current_shard = (self.current_shard + 1) % len(self.shards)
+            self.tokens = load_tokens(self.shards[self.current_shard])
+            self.current_position = B * T * self.process_rank
         return x, y
 
 # ----------------------------------------------------------
@@ -321,7 +335,7 @@ if master_process:
 
 
 # get a data batch
-train_loader = DataLoaderLite(B=B, T=T, process_rank=ddp_rank, num_processes=ddp_world_size)
+train_loader = DataLoaderLite(B=B, T=T, process_rank=ddp_rank, num_processes=ddp_world_size, split="train")
 # Batch size 16, 8 were too much for my 1660Ti
 # always choose these sizes in powers of 2
 # something like 17 runs inefficiently on the GPU
